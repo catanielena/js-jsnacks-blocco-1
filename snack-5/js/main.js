39d4fc745350485dfd016e86269f11b2446d39f0
catanielena <catani.elena1@gmail.com>
2021-08-26 10:41:17 +0200

    Refact: main_js

--- snack-5/js/main.js
+++ snack-5/js/main.js
@@ -1,15 +1,14 @@
 // Crea un array vuoto. 
 var box = [];
 var newNum = 0;
-// Chiedi per 6 volte all’utente di inserire un numero, 
-for (i=0; i<6; i++) {
+// Chiedi per 6 volte all’utente di inserire un numero, se è dispari inseriscilo nell’array
+for (var i=0; i<6; i++) {
     var newNum = parseInt(prompt("Inserisci un numero"));
     if (isNaN(newNum)) {
         var newNum = parseInt(prompt("Errore! Inserisci un numero"));
-    } else if (!(newNum % 2) == 0) {
+    } else if (newNum % 2 == 1) {
         box.push(newNum);
     }
 }
 
 console.log(box);
-// se è dispari inseriscilo nell’array
